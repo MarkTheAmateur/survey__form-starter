0b23f1378c0eff7958179d32556de18ec2d2908b
MarkTheAmateur <103880624+MarkTheAmateur@users.noreply.github.com>
2022-04-21 20:00:54 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// Code by Amateur Developer (Mark Nguyen).
+// Initially created for freeCodeCamp's project.
+
+// CREDITS:
+// - CDN Code Testing Kit by freeCodeCamp
+// - Background image by Daniel Leone on Unsplash. Link: https://unsplash.com/photos/g30P1zcOzXo?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+const projectName = 'survey-form';
